9d3ab13035ad80485848bd006ddbefd45073576c
katebee <whalenka88@gmail.com>
2016-02-05 15:58:07 +0000

    Functional clubs section: add index and show, correct naming and permissions

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,8 +1,25 @@
 class ClubsController < ApplicationController
+
+  def index
+    @clubs = Club.all
+  end
+
+  def show
+    @club = Club.find(params[:id])
+  end
+
+  def new
+  end
+
   def create
-    @club = Club.new(params[:club])
+    @club = Club.new(club_params)
 
     @club.save
     redirect_to @club
+  end
+
+  private
+  def club_params
+    params.require(:club).permit(:name, :website, :contact)
   end
 end
