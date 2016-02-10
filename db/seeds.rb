b355638b820aa6efa41cad9243cd42954a66a381
katebee <whalenka88@gmail.com>
2016-02-10 16:46:59 +0000

    Nokogiri seeds Club table from xml file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+xml = Nokogiri::XML(File.open("config/club_location.xml")) do |config|
+  config.options = Nokogiri::XML::ParseOptions::STRICT | Nokogiri::XML::ParseOptions::NONET
+end
+
+Club.delete_all
+
+xml.css("marker").each do |response_node|
+  Club.create(name: response_node["label"], location_lat: response_node["lat"], location_lng: response_node["lng"], postcode: response_node["Postcode"])
+end
