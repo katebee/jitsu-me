628e70afbe0dd783a9b3b774ea725d9ee7c72142
katebee <whalenka88@gmail.com>
2016-02-14 13:59:49 +0000

    Add regex for extracting session data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-xml = Nokogiri::XML(File.open("config/club_location.xml")) do |config|
+require 'open-uri'
+
+# Source and store data:
+clubxml = Nokogiri::XML(File.open("config/club_location.xml")) do |config|
   config.options = Nokogiri::XML::ParseOptions::STRICT | Nokogiri::XML::ParseOptions::NONET
 end
 
-Club.delete_all
+sessionxml = Nokogiri::HTML(open("http://www.jitsufoundation.org/Jiujitsu.asp?page=jujitsu_club&clubid=Wandsworth_Jitsu_Club"))
 
-xml.css("marker").each do |response_node|
+# Delete the current rows from the tables:
+Club.delete_all
+Event.delete_all
+
+# Populate the club table:
+clubxml.css("marker").each do |response_node|
   Club.create(name: response_node["label"], location_lat: response_node["lat"], location_lng: response_node["lng"], postcode: response_node["Postcode"])
 end
+
+# Experiments with storing session data:
+titlebox_grab = sessionxml.css("#titlebox2").last
+strong_grab = titlebox_grab.css("strong").to_s
+day_of_week = strong_grab[/(?<=<strong>)[a-zA-Z]{3}/]
+start_time = strong_grab[/(?<=,\s)\d{2}:\d{2}/]
+end_time = strong_grab[/(?<=\-\s)\d{2}:\d{2}/]
+
+# Populate the club table:
+Event.create(title: "Demo Event 1", description: day_of_week, start_time: start_time, end_time: end_time)
+Event.create(title: "Demo Event 2", description: day_of_week, start_time: start_time, end_time: end_time)
+Event.create(title: "Demo Event 3", description: day_of_week, start_time: start_time, end_time: end_time)
