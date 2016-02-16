0f65493167f835ac5746444b12557c28fff13fd3
katebee <whalenka88@gmail.com>
2016-02-16 12:59:55 +0000

    Calendar opens on week view and hides early morning times

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,19 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function(){
+  $('#calendar').fullCalendar({
+      defaultDate: moment(),
+      header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
+      },
+      defaultView: 'agendaWeek',
+      minTime: "10:00:00",
+      maxTime: "25:00:00",
+
+      events: '/events.json'
+  });
+});
