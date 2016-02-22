9e050494b7ba52529acf98d9800ad7d8aad55f74
katebee <whalenka88@gmail.com>
2016-02-22 22:58:46 +0000

    Update userMarker latlng on drag event

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -8,7 +8,7 @@ $(function(){
 $(document).ready(function(){
 
   var map = L.map('map').setView([51.505, -0.09], 13);
-  var userMarker;
+  var userMarker = new L.marker([], {draggable: true});
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
     maxZoom: 18,
@@ -18,24 +18,31 @@ $(document).ready(function(){
     id: 'mapbox.streets'
   }).addTo(map);
 
-  $(document).ready(function(){
-    $.get("/locations.json", function(data){
-      data.map(function(location) {
-        L.marker([location.location_lat, location.location_lng]).addTo(map).bindPopup(location.name);
-      })
-    })
+  $.get("/locations.json", function(data){
+    data.map(function(location) {
+      L.marker([location.location_lat, location.location_lng]).addTo(map).bindPopup(location.name);
+    });
   });
 
   $('#jitsu-me-button').on('click', function(){
     $(this).addClass('active');
     map.locate({setView: true});
-    map.removeLayer(userMarker);
+    if (userMarker) {
+      map.removeLayer(userMarker);
+    }
   });
 
   function onLocationFound(e) {
-    userMarker = new L.marker(e.latlng, {draggable: true});
+    userMarker.setLatLng(e.latlng);
     userMarker.addTo(map);
+    updateLocation(userMarker);
     $('#jitsu-me-button').removeClass('active');
+  }
+
+  function updateLocation(marker) {
+    marker.on('dragend', function(){
+      var position = marker.update();
+    });
   }
 
   function onLocationError(e) {
@@ -45,5 +52,4 @@ $(document).ready(function(){
 
   map.on('locationfound', onLocationFound);
   map.on('locationerror', onLocationError);
-
 });
